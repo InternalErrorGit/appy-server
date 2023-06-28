fe6600303ca71de3c79abeb3a469a083065b5a31
InternalError_ <phil.gatzka@gmail.com>
2023-06-28 10:54:49 +0200

    Initialized Project

---
+++ src/main/java/net/internalerror/appyserver/AppyServerApplication.java
@@ -0,0 +1,13 @@
+package net.internalerror.appyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/internalerror/appyserver/AppyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.internalerror.appyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
